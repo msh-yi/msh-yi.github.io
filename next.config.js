fef044a3de62ba8dfd1e174db5559ae031fe2812
msh-yi <marcus.sak@gmail.com>
2025-07-23 12:59:55 -0400

    static export

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    // Enable static HTML export on `next build`
+    output: 'export',
+  
+    // Optional: if you need clean URLs with trailing slashes
+    // trailingSlash: true,
+  
+    // Optional: customize the export directory
+    // distDir: 'dist',
+  };
+  
+  module.exports = nextConfig;
+  
